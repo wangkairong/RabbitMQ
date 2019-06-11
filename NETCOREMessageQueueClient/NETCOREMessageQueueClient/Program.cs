7955919d83bd686cbe76f5f6bc28a84729e91f06
wangkr <294242594@qq.com>
2019-06-11 15:16:49 +0800

    net core平台的RebbitMQ简单例子

---
+++ NETCOREMessageQueueClient/NETCOREMessageQueueClient/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace NETCOREMessageQueueClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exchangeName = "TestTopicChange";
+            string queueName1 = "NetCoreRabbotMQ1";
+            string queueName2 = "NetCoreRabbotMQ2";
+            string routeKey = "TestRouteKey.*";
+            //创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+               
+                HostName="127.0.0.1",
+                Port=5672
+            };
+            //创建连接
+            var connection = factory.CreateConnection();
+            //创建通道
+            var channel = connection.CreateModel();
+            //定义一个Direct类型交换机
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, false, false, null);
+            //声明队列
+            channel.QueueDeclare(queueName1, false , false, false, null);
+            //channel.QueueDeclare(queueName2, false, false, false, null);
+            //将队列绑定到交换机
+            channel.QueueBind(queueName1, exchangeName, routeKey, null);
+            //channel.QueueBind(queueName2, exchangeName, "", null);
+            //生成2个队列得消费者
+            //ConsumerGenerator(queueName1);
+            //ConsumerGenerator(queueName2);
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入Exit退出！");
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                var sendByte = Encoding.UTF8.GetBytes(input);
+                //发布消息
+                channel.BasicPublish(exchangeName, "TestRouteKey.one",null,sendByte);
+            } while (input.Trim().ToLower()!="exit");
+
+            channel.Close();
+            connection.Close();
+        }
+
+        private static void ConsumerGenerator(string queueName1)
+        {
+            //创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                Port = 5672
+
+            };
+            //创建连接
+            var conn = factory.CreateConnection();
+            //创建通道
+            var channel = conn.CreateModel();
+            //事件基本消费者
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            //接收到消息事件
+
+            consumer.Received += (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine($"Queue:{queueName1}收到消息：{message}");
+                //确认消息已被消费
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            //启动消费这 设置为手动应答消息
+            channel.BasicConsume(queueName1, false, consumer);
+            Console.WriteLine($"Queue:{queueName1},消费者已启动");
+        }
+    }
+}
---
+++ NETCOREMessageQueueServer/NETCOREMessageQueueServer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace NETCOREMessageQueueServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                Port=5672
+                
+            };
+            //创建连接
+            IConnection conn = factory.CreateConnection();
+            //创建通道
+            IModel channel = conn.CreateModel();
+
+
+            #region 不使用using
+
+            //事件基本消费者
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            //接收到消息事件
+            consumer.Received += (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine($"收到消息：{message}");
+
+               // Console.WriteLine($"收到该消息[{ea.DeliveryTag}]延迟10S发送回执");
+               // Thread.Sleep(1000);
+                //确认消息已被消费
+                channel.BasicAck(ea.DeliveryTag, false);
+                //Console.WriteLine($"已发送回执[{ea.DeliveryTag}]");
+            };
+            //启动消费者 设置手动应答消息
+            channel.BasicConsume("NetCoreRabbotMQ1", false, consumer);
+            Console.WriteLine("消费者启动成功！");
+            Console.ReadKey();
+            channel.Close();
+            conn.Close();
+            #endregion
+
+
+
+            #region 使用using
+            //using (IConnection conn2 = factory.CreateConnection())
+            //{
+            //    using (IModel channel2 = conn2.CreateModel())
+            //    {
+            //        //在MQ上定义一个持久化队列，如果名称相同不会重复创建
+            //        channel2.QueueDeclare("NetCoreRabbotMQ", false, false, false, null);
+            //        //输入1，那如果接收一个消息，但是没有应答，则客户端不会收到下一个消息
+            //        channel2.BasicQos(0, 1, false);
+            //        Console.WriteLine("Listening...");
+
+            //        //在队列定义一个消费者
+            //        QueueingBasicConsumer consumer2 = new QueueingBasicConsumer(channel2);
+            //        //消费队列 并设置为主动应答模式
+            //        channel2.BasicConsume("NetCoreRabbotMQ", false, consumer2);
+
+
+            //        while (true)
+            //        {
+            //            //阻塞函数 获取队列数据
+            //            BasicDeliverEventArgs eventArgs = consumer2.Queue.Dequeue();
+
+            //            byte[] bytes = eventArgs.Body;
+            //            string str = Encoding.UTF8.GetString(bytes);
+
+            //            Console.WriteLine("读取队列的消息：" + str);
+            //            //回复确认
+            //            channel2.BasicAck(eventArgs.DeliveryTag, false);
+            //        }
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
